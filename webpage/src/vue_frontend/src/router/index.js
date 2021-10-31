7dfbf3c9beff6f4290cb5fe2f335a4865b729871
Anton Malke <malke@codeguides.de>
2021-10-31 22:24:29 +0100

    Vue 404 ErrorPage

--- webpage/src/vue_frontend/src/router/index.js
+++ webpage/src/vue_frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    // not found
+    path: '*',
+    name: 'page_not_found',
+    
+    component: PageNotFound
   }
 ]
 
---
+++ webpage/src/vue_frontend/src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="PageNotFound">
+    <h1>PageNotFound</h1>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'PageNotFound',
+  }
+</script>

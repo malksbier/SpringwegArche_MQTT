f03df201b5e7b568db66ee82209694613da0b095
Anton Malke <malke@codeguides.de>
2021-11-01 01:43:19 +0100

    Axios

--- webpage/src/vue_frontend/src/App.vue
+++ webpage/src/vue_frontend/src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
   <v-app>
     <v-app-bar
@@ -19,5 +20,20 @@ export default {
   data: () => ({
     //
   }),
+
+  created() {
+     this.$axios.post('http://localhost:8080/login/register', {
+   
+        username: "Adminn",
+        password: "Adminn",
+    
+      }).then(function (response) {
+        console.log(response);
+      }).catch(function (error) {
+        console.log(error);
+      }).then(function () {
+        console.log("done");
+      });
+  }
 };
 </script>

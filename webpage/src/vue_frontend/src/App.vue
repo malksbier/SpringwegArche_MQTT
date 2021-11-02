195631f50c9ebaa45ada159f28d26f5ac7d298c4
Anton Malke <malke@codeguides.de>
2021-11-02 23:43:10 +0100

    BASIC PAGE

--- webpage/src/vue_frontend/src/App.vue
+++ webpage/src/vue_frontend/src/App.vue
@@ -9,27 +9,7 @@
       class="deep-purple accent-4 px-4"
       dark  v-bind:width="200"
       permanent expand-on-hover>
-      <v-list>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-           @click="switchPageSafe(item.path)">
-          <v-list-item-icon class="v-list-item__icon-same-width">
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content link>  
-            <v-list-item-title>{{ $t(item.title) }}</v-list-item-title>
-          </v-list-item-content>
       
-        </v-list-item>
-
-        <language-switcher></language-switcher>
-
-        <v-btn block>
-            Logout
-          </v-btn>
-      </v-list>
     </v-navigation-drawer>
      
     -->   
@@ -48,28 +28,21 @@
 
 <script>
 import Navbar from './components/Navbar.vue';
-import LanguageSwitcher, { methods } from './components/LanguageSwitcher.vue';
+
 import {getBrowserLocale} from "@/util/i18n/LocalsUtil"
-import {switchPageSafely} from "@/util/RouterUtil"
+
 import PageNotFound from './views/PageNotFound.vue';
 import NavbarDesign from './components/Navbar.vue';
 
 
 export default {
   components: { 
-    //LanguageSwitcher, 
     NavbarDesign,
-    //PageNotFound
-     },
+  },
   name: 'App',
   
   data: () => ({
-    user : 0, // 0 means null
-    displayAppBar : true,
-    items: [
-          { title: 'main_page', icon: 'fas fa-home' ,path: "/"},
-          { title: 'about', icon: 'fas fa-address-card' ,path: "/about"},
-        ],
+    user : "0", // 0 means null
   }),
 
   created() {
@@ -92,9 +65,7 @@ export default {
     */
   },
   methods: {
-    switchPageSafe(path) {
-      switchPageSafely(this.$route, this.$router,path)
-    },
+    
   }
 };
 </script>
--- webpage/src/vue_frontend/src/components/Navbar.vue
+++ webpage/src/vue_frontend/src/components/Navbar.vue
@@ -5,42 +5,73 @@
                 <v-icon> fas fa-bars </v-icon>
             </v-app-bar-nav-icon>
             <v-spacer></v-spacer>
-            <v-toolbar-title> APPNAME </v-toolbar-title>
+            <v-toolbar-title> {{ $t("appname") }} </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-spacer></v-spacer>
             <v-btn light v-if="user != 0" @click="signOut();"> 
-                <span>sign_out</span>
+                <span> {{ $t("sign_out") }} </span>
                 <v-icon right>fas fa-sign-out-alt</v-icon>
             </v-btn>
             <v-btn light  v-if="user == 0" @click="signIn();">
-                <span>sign_in</span>
+                <span> {{ $t("sign_in") }} </span>
                 <v-icon right>fas fa-sign-in-alt</v-icon>
             </v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer app v-model="showNavigationDrawer" > 
+        <v-navigation-drawer app v-model="showNavigationDrawer"> 
             <div style="height: 100%" @mouseleave="hideNavigationDrawer()">
-                <p >NAVIGATION</p>
-            </div>  
+            <v-list>
+             
+                    <h4 class="px-4 pt-6">{{ $t("navigation") }}</h4>
+             
+                
+
+                <v-divider></v-divider>
+                <v-list-item
+                    v-for="route in routes"
+                    :key="route.title"
+                    @click="switchPageSafe(route.path)">
+                        <v-list-item-icon class="v-list-item__icon-same-width">
+                            <v-icon>{{ route.icon }}</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content link>  
+                            <v-list-item-title>{{ $t(route.title) }}</v-list-item-title>
+                        </v-list-item-content>
+      
+                </v-list-item>
+                <v-divider></v-divider>
+
+                <language-switcher></language-switcher>
+
+            </v-list>
+            </div>
         </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
+import {switchPageSafely} from "@/util/RouterUtil"
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue';
+
 export default {
     props: {
         user: {
             required: true
         }
     },
-        
-  
-    computed: {
-
+    components: {
+        LanguageSwitcher
     },
+
     data() {
         return {
             showNavigationDrawer: false,
+
+            routes: [
+                { title: 'main_page', icon: 'fas fa-home' ,path: "/"},
+                { title: 'about', icon: 'fas fa-address-card' ,path: "/about"},
+        ],
         }
     },
     methods: {
@@ -48,14 +79,17 @@ export default {
             this.showNavigationDrawer = false;
         },
         signIn() {
-            console.log("signin");
+            //console.log("signin");
             var myUser = 1;
             this.$emit("update-user", myUser);
         },
         signOut() {
-            console.log("signOut");
+            //console.log("signOut");
             var myUser = 0;
             this.$emit("update-user", myUser);
+        },
+        switchPageSafe(path) {
+            switchPageSafely(this.$route, this.$router,path)
         },
     }
 }

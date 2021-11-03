87621ed7e259d0078af2e6fcfcce3353caad927f
Anton Malke <malke@codeguides.de>
2021-11-03 03:50:56 +0100

    PageNotFound Update

--- webpage/src/vue_frontend/src/views/PageNotFound.vue
+++ webpage/src/vue_frontend/src/views/PageNotFound.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="PageNotFound">
-    <h1>PageNotFound</h1>
+  <div class="PageNotFound" style="height: 100%">
+
+    <v-layout justify-center style="padding-top: 50px" class="">
+ 
+        <v-col>
+          <h1 style="text-align: center;"> {{ $t("page_not_found") }} </h1>
+          <p style="text-align: center;"> {{ $t("page_not_found_hint") }} </p>
+          <v-icon x-large color="red lighten-2" style="width:100%;">fas fa-exclamation-triangle</v-icon>
+        </v-col>
+      
+      
+    </v-layout>
+    
   </div>
 </template>
 

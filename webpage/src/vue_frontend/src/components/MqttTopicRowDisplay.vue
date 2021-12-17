dd6cbbfe91cffa6821559045b9adc8e7261f4bfd
Anton Malke <malke@codeguides.de>
2021-12-17 04:16:02 +0100

    Displaying or not

--- webpage/src/vue_frontend/src/components/MqttTopicRowDisplay.vue
+++ webpage/src/vue_frontend/src/components/MqttTopicRowDisplay.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="mqtt-topic-row-display" style = style>
     <v-row>
-  
       <p> {{mqttTopic.topicName}} </p>
       
       <div v-if="mqttTopic.children.length > 0" >
-        <p> {{mqttTopic.topicName}} </p>
+        <v-button icon  v-ripple="false" @click="toggleChildrenDisplay();" v-if="!displayChildren">
+          <v-icon class="toggle-switch">fas fa-caret-up</v-icon>
+        </v-button>
+
+        <v-button icon v-ripple="false" @click="toggleChildrenDisplay();" v-if="displayChildren"> 
+          <v-icon class="toggle-switch">fas fa-caret-down</v-icon> 
+        </v-button>
+        
       </div>
       
     </v-row>
     
-    <div v-for="mqttTop in mqttTopic.children" :key = mqttTop.id>
-      <MqttTopicRowDisplays :mqttTopic = mqttTop :marginLeft = 20></MqttTopicRowDisplays>
+    <div v-if="displayChildren">
+      <div v-for="mqttTop in mqttTopic.children" :key = mqttTop.id>
+        <MqttTopicRowDisplays :mqttTopic = mqttTop :marginLeft = 20></MqttTopicRowDisplays>
+      </div>
     </div>
   </div>
 </template>
@@ -36,15 +44,20 @@ export default {
       required: true
     }
   },
+
+  data() {
+    return {
+      displayChildren: true,
+    }
+  },
   mounted() {
     console.log("topic");
     console.log(JSON.stringify(this.mqttTopic.children))
   },
-  data: function() {
-  
-  },
   methods: {
-    
+    toggleChildrenDisplay() {
+      this.displayChildren = !this.displayChildren;
+    }
   }
 
   
@@ -56,4 +69,10 @@ export default {
 .mqtt-topic-row-display {
   margin-left: 5px;
 }
+.toggle-switch {
+  bottom: 2px;
+  padding-left: 5px;
+  color: rgba(0,0,0,.95);
+}
+
 </style>
